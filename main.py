bcaed8d7e406fb1887fa423b2c7c2df49e09633b
chrisabdo11 <christopherabdo@yahoo.com>
2026-02-24 19:10:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ df2 = filter_by_surface (df2,surface)
 df_total = df1.merge(df2, how = 'inner', on  = ["Player"])
 while True:
     name_A = input("Enter the name of the first player : ").strip()
-    playerA = get_player_row(df_total, name_A)
+    player_A = get_player_row(df_total, name_A)
     
-    if playerA is None:
+    if player_A is None:
         print("The first player is unfindable ! Try again.\n")
     else:
         break 
@@ -28,12 +28,12 @@ while True:
     name_B = input("Enter the name of the second player : ").strip()
     playerB = get_player_row(df_total, name_B)
     
-    if playerB is None:
+    if player_B is None:
         print("The second player is unfindable ! Try again.\n")
     else:
         break
-player_A = Player(name_A,playerA)
-player_B = Player(name_B,playerB)
+player_A = Player(name_A,player_A)
+player_B = Player(name_B,player_B)
 predictor = MatchPredictor(df_total)
 result = predictor.predict_winner(name_A, name_B, surface)
 
@@ -46,12 +46,3 @@ print("{} : {:.2%}".format(clean_name(name_B).title(), probB))
 result2 = predictor.win_set_odds(name_A,name_B,surface)
 print("\nResults:")
 print("odds of ",clean_name(name_A).title()," winning a set are:", result2)
-
-
-
-
-
-
-
-
-
